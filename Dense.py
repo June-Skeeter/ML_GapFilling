ec60ef8ba8779f0932a2637f0c7f1fd5aa2949c6
Ubuntu <wesley.skeeter@alumni.ubc.ca>
2018-05-09 09:12:18 +0000

    Updated CV Scoring

--- Dense.py
+++ Dense.py
@@ -7,26 +7,27 @@ from matplotlib import pyplot as plt
 def Params(Func,Y,MP = True):
     params = {}
     if Func == 'Full':
-        epochs = 600#1000
-        reps = 15
-        N_Max = 100#200
-        N_min = 2
+        epochs = 500#1000
+        reps = 30
+        N_Max = 150#200
+        N_min = 5
         # T_Max = 48
-        samp_size = 5
+        samp_size = 4
         Searches = 3
         params['proc']=3
     elif Func == 'Test':
-        epochs = 20
+        epochs = 100
         reps = 4
         N_Max = 100
-        N_min = 5
+        N_min = 50
         # T_Max = 10
-        samp_size = 3
+        samp_size = 2
         Searches = 2
         params['proc']=3
     if MP == False:
         params['proc']=1
-    N = np.array(np.random.rand(samp_size)*N_Max+N_min,dtype='int32')
+    # N = np.array(np.random.rand(samp_size)*N_Max+N_min,dtype='int32')
+    N = np.linspace(N_min,N_Max,samp_size,dtype='int32')
     # T = np.array(np.random.rand(samp_size)*T_Max,dtype='int32')
     d = {'N':N}
     Runs = pd.DataFrame(data=d)
@@ -39,28 +40,6 @@ def Params(Func,Y,MP = True):
     params['Searches']=Searches
     return(Runs,params)
 
-# def LSTM_Model(Neurons,batch_size,time_steps,inputs,lr=1e-4,Memory=.9):
-#     import keras
-#     from keras.models import Sequential
-#     from keras.layers import Dense
-#     from keras.layers import LSTM
-#     from keras.wrappers.scikit_learn import KerasRegressor
-#     from keras.callbacks import EarlyStopping,ModelCheckpoint,LearningRateScheduler
-#     import tensorflow as tf
-#     config = tf.ConfigProto()
-#     config.gpu_options.per_process_gpu_memory_fraction = Memory
-#     session = tf.Session(config=config)
-#     model = Sequential()
-#     model.add(LSTM(Neurons, input_shape=(time_steps,inputs),stateful = False))
-#     model.add(Dense(1))
-#     NUM_GPU = 1 # or the number of GPUs available on your machine
-    
-#     adam = keras.optimizers.Adam(lr = lr)
-#     gpu_list = []
-#     for i in range(NUM_GPU): gpu_list.append('gpu(%d)' % i)
-#     model.compile(loss='mean_squared_error', optimizer='adam')#,context=gpu_list) # - Add if using MXNET
-#     return(model)
-
 def Dense_Model(Neurons,batch_size,inputs,lr=1e-4,Memory=.9):
     import keras
     from keras.models import Sequential
@@ -72,7 +51,7 @@ def Dense_Model(Neurons,batch_size,inputs,lr=1e-4,Memory=.9):
     config.gpu_options.per_process_gpu_memory_fraction = Memory
     session = tf.Session(config=config)
     model = Sequential()
-    model.add(Dense(Neurons, input_dim=inputs,activation='sigmoid'))
+    model.add(Dense(Neurons, input_dim=inputs,activation='relu'))
     model.add(Dense(1))
     NUM_GPU = 1 # or the number of GPUs available on your machine
     
@@ -89,10 +68,10 @@ def Train_Steps(epochs,Neurons,X_train,X_test,X_val,y_train,y_test,y_val,i,X_fil
     lr = 1e-3
     Mod = Dense_Model(Neurons,X_train.shape[0],X_train.shape[1],lr=lr,Memory=Memory)
     killscore=0
-    killmax = 10
+    killmax = 5
     e = 0
     udate = 3
-    batch_size=100## Arbitrarily set to 10, can modify later
+    batch_size=100
     while killscore < killmax and e < epochs:
         Mod.fit(X_train,y_train,batch_size=batch_size, epochs=1,shuffle=True,verbose=0)
         old_weights = Mod.get_weights()
@@ -117,4 +96,4 @@ def Train_Steps(epochs,Neurons,X_train,X_test,X_val,y_train,y_test,y_val,i,X_fil
     MSE = metrics.mean_squared_error(y_val,Yval)
     Scorez=np.asanyarray(Scorez)
     y_fill = Mod.predict(X_fill,batch_size=batch_size)
-    return(MSE,y_fill)
+    return(MSE,y_fill,Yval,y_val)
